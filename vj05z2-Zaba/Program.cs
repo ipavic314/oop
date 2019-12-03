a07bd58445cc7172285b87fffe92e2efea06a359
ipavic314 <49288091+ipavic314@users.noreply.github.com>
2019-12-03 16:21:17 +0100

    Add files via upload

---
+++ vj05z2-Zaba/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vj05z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zaba z1 = new Zaba(5, "zelena");
+            Zaba z2 = new Zaba(5, "zelena");
+            Console.WriteLine(z1);
+            Console.WriteLine(z2);
+
+            Console.WriteLine(z1.Equals(z2));
+
+            HashSet<Zaba> zabe = new HashSet<Zaba> { z1, z2 };
+
+            foreach (var item in zabe)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ vj05z2-Zaba/Zaba.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vj05z2
+{
+    class Zaba
+    {
+        public Zaba(int brojNogu, string boja)
+        {
+            BrojNogu = brojNogu;
+            Boja = boja;
+        }
+
+        public int BrojNogu { get; set; }
+        public string Boja { get; set; }
+
+
+        public override string ToString() => $"Broj nogu:{BrojNogu}, Boja: {Boja}";
+
+        /* public override bool Equals(object obj)
+         {
+             if (obj is Zaba other)
+             {
+                 return BrojNogu == other.BrojNogu && Boja == other.Boja;
+             }
+             else
+             {
+                 return false;
+             }
+         }*/
+
+        // drugi nacin 
+        public override bool Equals(object obj) => obj is Zaba other ? BrojNogu == other.BrojNogu && Boja == other.Boja : false;
+
+
+        public override int GetHashCode()
+        {
+            return BrojNogu.GetHashCode() + Boja.GetHashCode();
+        }
+
+
+
+    }
+}
